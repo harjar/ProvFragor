e2c96b09f1846f1a7bf666acddd3fd237c93b0dd
harjar <44702232+harjar@users.noreply.github.com>
2025-03-20 10:49:33 +0100

    Förslag på kodlösningar till provet

--- src/feltSumma/FeltSumma.java
+++ src/feltSumma/FeltSumma.java
@@ -7,7 +7,7 @@ package feltSumma;
 public class FeltSumma {
 
     public static void main(String[] args) {
-        int[] heltal = {12, 16, 78, 98, 2, 45, 65};
+        int[] heltal = {12, 16, 78, 98, 2, 45, 65, 45, 89};
         int summa = 0;
 
         for (int i = 0; i < heltal.length; i++) {

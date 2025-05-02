9f0f7eacfc0b74018eb8ef058d63d0336977bbd0
harjar <44702232+harjar@users.noreply.github.com>
2025-05-02 13:59:46 +0200

    Test

--- src/asterixtriangel/AsterixTriangel.java
+++ src/asterixtriangel/AsterixTriangel.java
@@ -14,12 +14,25 @@ public class AsterixTriangel {
         System.out.print("Ange antalet rader trangeln ska ha: ");
         int rader = input.nextInt();
 
+        /*
         System.out.println("");
         for (int i = 1; i <= rader; i++) {
             for (int j = 0; j < rader - i; j++) {
                 System.out.print(" ");
             }
             for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+         */
+        System.out.println("");
+        for (int i = 0; i <= rader; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= rader - i; k++) {
                 System.out.print("*");
             }
             System.out.println("");

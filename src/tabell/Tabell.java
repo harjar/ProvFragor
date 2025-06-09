39b3eb5f827e3f28cbd3a9590e4f4d2cd42e1a95
harjar <44702232+harjar@users.noreply.github.com>
2025-06-09 20:43:42 +0200

    Klart

--- src/tabell/Tabell.java
+++ src/tabell/Tabell.java
@@ -9,10 +9,12 @@ public class Tabell {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 10; i++) {
-            for (int j = 1; j <= 10; j += 2) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("" + (j * i) + "\t");
             }
             System.out.println("");
         }
+        System.out.println("");
+        System.out.println("");
     }
 }

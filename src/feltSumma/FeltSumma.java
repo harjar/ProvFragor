f2ab33511ab13f708f0f29220ce4473b6b131935
jarhar <jarhar@GAFLHGY8028.adm.huddinge.se>
2026-02-03 08:37:37 +0100

    Medel coh feltsumma klart

--- src/feltSumma/FeltSumma.java
+++ src/feltSumma/FeltSumma.java
@@ -1,5 +1,7 @@
 package feltSumma;
 
+import java.util.Scanner;
+
 /**
  *
  * @author jarih
@@ -8,11 +10,20 @@ public class FeltSumma {
 
     public static void main(String[] args) {
         int[] heltal = {12, 16, 78, 98, 2, 45, 65};
-        int summa = 0;
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Ange en mulipilkator: ");
+        int mul = input.nextInt();
+        multiplicera(heltal, mul);
         for (int i = 0; i < heltal.length; i++) {
-            summa += heltal[i];
+            System.out.print(heltal[i] + " ");
         }
-        System.out.println("Summa = " + summa);
+        System.out.println("");
+    }
+
+    private static void multiplicera(int[] heltal, int mul) {
+        for (int i = 0; i < heltal.length; i++) {
+            heltal[i] *= heltal[i];
+        }
     }
 }

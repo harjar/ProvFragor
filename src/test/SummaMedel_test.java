054a6e670411edc9ce046b416a0ceef38e52cf7a
harjar <44702232+harjar@users.noreply.github.com>
2026-02-09 21:06:18 +0100

    Summa och medel. Frågor på felet, samt hur lösningen ser ut revidering av inatningen

---
+++ src/test/SummaMedel_test.java
@@ -0,0 +1,40 @@
+/*
+    Vad blir fel i programkörningen
+    Vilken typ av fel ser du i programmet nedan?
+    ??Hur löser du felet, ange vad och vilken/vilka rad/rader du behöver skriva om för att programmet ska ge korrekt resultat.
+    Ange vad du behöver göra för att programmet ska göra korrekt.
+ */
+package test;
+
+import summaMedel.*;
+import java.util.Scanner;
+
+public class SummaMedel_test {
+
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int[] heltal = new int[6];
+        int tal;
+        int antal = 0;
+        int summa = 0;
+        double medel;
+        do {
+            System.out.print("Ange ett heltal (avsluta med ett negativt tal): ");
+            tal = input.nextInt();
+            if (tal >= 0) {
+                heltal[antal++] = tal;
+            } else {
+                break;
+            }
+
+        } while (tal >= 0 && antal < heltal.length);
+
+        for (int i = 0; i < heltal.length; i++) {
+            summa += heltal[i];
+        }
+
+        medel = summa / (double) heltal.length;
+        System.out.println("Summa: " + summa);
+        System.out.println("Medel: " + medel);
+    }
+}
